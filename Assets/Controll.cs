6c48c00ea6a6e6261c54247a37904e158077ce6c
krasnoyar <stepan.kulakovvv.777@gmail.com>
2019-03-19 16:12:32 +1000

    Initial commit

---
+++ Assets/Controll.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controll : MonoBehaviour {
+
+    public float speed = 1.0f;
+	void Awake () {
+		
+	}
+
+}
