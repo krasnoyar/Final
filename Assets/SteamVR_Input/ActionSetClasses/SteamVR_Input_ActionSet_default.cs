8c0b0411b10192bac3f6c94caddf7c6c12febcd4
krasnoyar <stepan.kulakovvv.777@gmail.com>
2019-03-22 12:42:11 +1000

    final commit

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -89,6 +89,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean t
+        {
+            get
+            {
+                return SteamVR_Actions.default_t;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
